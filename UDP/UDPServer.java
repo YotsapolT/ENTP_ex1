cefd2f52fd314195aca8c5c76a1d679c1d096944
Tong <yotsapol.tong@gmail.com>
2023-12-27 12:03:44 +0700

    update udp

--- UDP/UDPServer.java
+++ UDP/UDPServer.java
@@ -1,5 +1,6 @@
-import java.io.*;
 import java.net.*;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 class UDPServer {
   public static void main(String args[]) throws Exception {
@@ -9,13 +10,17 @@ class UDPServer {
       byte[] receiveData = new byte[1024];
       byte[] sendData = new byte[1024];
       System.out.println("The server is waiting ");
+      // receive empty str
       DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
       serverSocket.receive(receivePacket);
-      String sentence = new String(receivePacket.getData());
       InetAddress IPAddress = receivePacket.getAddress();
       int port = receivePacket.getPort();
-      String capitalizedSentence = sentence.toUpperCase();
-      sendData = capitalizedSentence.getBytes();
+      // process str
+      Date date = new Date();
+      SimpleDateFormat formatter = new SimpleDateFormat("E, MM/dd/YYYY HH:mm:ss z");
+      String strDate = formatter.format(date);
+      // send str
+      sendData = strDate.getBytes();
       DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
       serverSocket.send(sendPacket);
     }
